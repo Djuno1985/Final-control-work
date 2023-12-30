d9d5cc18c4774334ed5026ccd6f29498cc926fc7
Юлия Корнилова <uliakornilova@MacBook-Pro-Ulia.local>
2023-12-30 18:50:21 +0300

    inputArray

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите строки через запятую: ");
+        string input = Console.ReadLine();
+
+        string[] inputArray = input.Split(',');
+
+        string[] resultArray = FilterStrings(inputArray);
+
+        Console.WriteLine("Результат:");
+        foreach (string result in resultArray)
+        {
+            Console.WriteLine(result);
+        }
